e88e0ec1907ae96a882c829213f0b8eac3c7f122
BBOLL <79545134+bindubollineni@users.noreply.github.com>
2021-08-19 15:54:14 -0400

    Update TC_004.java

--- src/test/java/testCases/TC_004.java
+++ src/test/java/testCases/TC_004.java
@@ -2,4 +2,8 @@ package testCases;
 
 public class TC_004 {
 //This Test is implemented in future
+  public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My git changes Program");
+	}
 }
